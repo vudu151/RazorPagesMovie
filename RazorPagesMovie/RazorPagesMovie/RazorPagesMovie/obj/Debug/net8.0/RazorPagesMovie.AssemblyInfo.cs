b5dc7c07646f8e36b8e9ca8ee0389a535542353b
duvxonenet <duvx@onenet.vn>
2024-11-20 08:09:15 +0700

    add update js in Index

--- RazorPagesMovie/RazorPagesMovie/RazorPagesMovie/obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
+++ RazorPagesMovie/RazorPagesMovie/RazorPagesMovie/obj/Debug/net8.0/RazorPagesMovie.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35d4d2cf52c08dbe0ef8a05c89832e4d75caff29")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f973393d5875c7c8035a47b1878a3403a2a8763")]
 [assembly: System.Reflection.AssemblyProductAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyTitleAttribute("RazorPagesMovie")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
